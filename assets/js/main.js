e305cdf8358ce5505e40d389e87d1ad972a38c77
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-03-30 17:55:01 +0200

    Exercise ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,11 @@
 var userFound = false;
 // 1 Chiedi all’utente la sua email,
 var mailUtente = prompt('Inserisci la tua email.');
-// console.log(mailUtente);
+console.log(mailUtente);
 
 //creo il contenitore di 'email'
 var indirizziMail = ['michele@boolean.careers', 'fabio@boolean.careers', 'roberto@boolean.careers'];
-// console.log(indirizziMail);
+console.log(indirizziMail);
 
 // 2 controlla che sia nella lista di chi può accedere,
 for(var i = 0; i < indirizziMail.length; i++) {
@@ -25,3 +25,35 @@ if(userFound == true) {
   document.getElementById('search-array').classList.add('bg-usuccess');
   console.log('utente non trovato');
 }
+
+
+//Esercizio 2 : 'Gioco dei dadi'
+//generare un numero random da 1  a 6, sia per il giocatore sia per il computer
+
+var bottoneGioca = document.getElementById('btn-play');
+bottoneGioca.addEventListener('click',
+  function() {
+
+    var computer = Math.floor(Math.random()*6) +1;
+    document.getElementById('computer-score').innerHTML = computer;
+    console.log('punteggio pc: ' + computer);
+    var player = Math.floor(Math.random()*6) +1;
+    document.getElementById('player-score').innerHTML = player;
+    console.log('punteggio giocatore: ' + player)
+
+    if( computer > player ) {
+      document.getElementById('game-text').innerHTML = 'Computer';
+      console.log('Ha vinto il pc!');
+    } else if ( computer < player ){
+      document.getElementById('game-text').innerHTML = 'Utente';
+      console.log('Hai vinto!');
+    } else {
+      document.getElementById('game-text').innerHTML = 'Pareggio';
+      console.log('Avete pareggiato!')
+    }
+    document.getElementById('dice-img').classList.add('animation-fast');
+  }
+);
+
+
+//Stabilire il vincitore, in base a chi fa il punteggio più alto
